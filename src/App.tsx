c2c9e4f89932409249fcedffa5db83c10d2bde11
Jacob Mellin <mail@jacobmellin.de>
2023-12-28 21:43:34 +0100

    Enable custom app close button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { exit } from '@tauri-apps/api/process';
+
 
 function App() {
   return (
     <main>
-        <a href="#" class="close-btn absolute bg-red-400 right-5 top-5 rounded-full w-4 h-4 hover:bg-red-200 transition-colors"></a>
+        <a href="#" onClick={async () => { await exit(1); alert("hi"); }} class="close-btn absolute bg-red-400 right-5 top-5 rounded-full w-4 h-4 hover:bg-red-200 transition-colors"></a>
         <div class="container px-8 py-8 select-none cursor-default">
           <h1 class="text-4xl font-light text-slate-400">Goal</h1>
         </div>

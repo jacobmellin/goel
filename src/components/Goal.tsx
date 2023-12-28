447ff549a5e54b79ab5c1df7329dfa9571090ecd
Jacob Mellin <mail@jacobmellin.de>
2023-12-29 00:28:50 +0100

    Add trash button

--- src/components/Goal.tsx
+++ src/components/Goal.tsx
@@ -1,3 +1,6 @@
+import TrashIcon from 'eva-icons/outline/svg/trash-outline.svg';
+
+
 type TrackingInterval = "monthly" | "daily" | "weekly";
 
 interface GoalProps {
@@ -8,8 +11,11 @@ interface GoalProps {
 function Goal(props: GoalProps) {
     return (
         <div class="bg-slate-800/40 rounded-md border-white/10 border shadow text-neutral-200 flex flex-col hover:border-orange-300/50 hover:bg-slate-700/20 hover:cursor-pointer transition-colors">
-            <div class="p-4 font-bold flex-1">{ props.text }</div>
-            <div class="p-2 text-slate-600 tracking-wider font-normal text-sm uppercase border-t-white/10 border-t">{ props.trackingInterval }</div>
+            <div class="p-4 font-bold flex-1">{props.text}</div>
+            <div class="py-2 px-3 flex w-full border-t-white/10 border-t">
+                <div class="text-slate-600 tracking-wider font-normal text-sm uppercase flex-1">{props.trackingInterval}</div>
+                <TrashIcon class="relative w-5 fill-red-400/50" />
+            </div>
         </div>
     );
 }

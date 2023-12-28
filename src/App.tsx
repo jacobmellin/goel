b221fd6dbae2fb3bc21d4e15ecf003c17bf94d8c
Jacob Mellin <mail@jacobmellin.de>
2023-12-28 21:55:10 +0100

    Set app title to goel

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <main>
         <a href="#" onClick={async () => { await exit(1); alert("hi"); }} class="close-btn absolute bg-red-400 right-5 top-5 rounded-full w-4 h-4 hover:bg-red-200 transition-colors"></a>
         <div class="container px-8 py-8 select-none cursor-default">
-          <h1 class="text-3xl tracking-wide font-light text-slate-400 uppercase">Goal</h1>
+          <h1 class="text-3xl tracking-wide font-light text-slate-400 uppercase">Goel</h1>
         </div>
     </main>
   );

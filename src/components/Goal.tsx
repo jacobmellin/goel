783f6f99f40eee338f4dda526836571254a6aefd
Jacob Mellin <mail@jacobmellin.de>
2023-12-28 23:47:46 +0100

    Add slot for goal component

---
+++ src/components/Goal.tsx
@@ -0,0 +1,17 @@
+type TrackingInterval = "monthly" | "daily" | "weekly";
+
+interface GoalProps {
+    text: string,
+    trackingInterval: TrackingInterval
+}
+
+function Goal(props: GoalProps) {
+    return (
+        <div class="bg-slate-800/40 rounded-md border-white/10 border shadow text-neutral-200 flex flex-col">
+            <div class="p-4 font-bold flex-1">{ props.text }</div>
+            <div class="p-2 text-slate-600 tracking-wider font-normal text-sm uppercase border-t-white/10 border-t">{ props.trackingInterval }</div>
+        </div>
+    );
+}
+
+export default Goal;

2385e4800104d6724b752c45fd22be2b06f02aca
Jacob Mellin <mail@jacobmellin.de>
2023-12-29 18:10:54 +0100

    Add seperate add to goal modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import AddGoalModal from "./components/AddGoalModal";
 
 import GoalsView from "./components/GoalsView";
 import Header from "./components/Header";
@@ -11,7 +12,7 @@ function App() {
             <div class="flex-1 px-6 overflow-y-scroll select-none cursor-default z-0">
                 <GoalsView />
             </div>
-            <Modal title="Add a new goal" visible={false} />
+            <AddGoalModal />
             <footer class="text-xs text-center text-calm-500/50 px-2 mx-auto mb-4">Made with heart by jacobmellin | Please consider supporting: jacobmellin</footer>
         </main>
     );
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,21 +1,16 @@
-import ButtonPrimary from "./ButtonPrimary";
-import Button from "./Button.tsx";
-import TextInput from "./TextInput.tsx";
-
 interface ModalProps {
     allowBgClose?: boolean,
     visible?: boolean,
-    title: string
+    title: string,
+    children?: any[]
 }
 
 export default function Modal(props: ModalProps) {
     return <div classList={{ hidden: !props.visible }} class="flex px-4 flex-col justify-center items-center absolute pt-24 top-1 left-1 bottom-1 right-1 rounded-2xl backdrop-blur-lg bg-slate-900/10 select-none cursor-default">
         <div class="p-6 bg-gaze-900 rounded-lg w-full max-w-xl shadow-lg border border-white/10">
             <h2 class="font-bold text-lg text-orange-200">{props.title}</h2>
-            <TextInput label="Goal Description" placeholder="Enter your goal here" />
-            <div class="flex mt-4 pt-2 gap-2 flex-row justify-space-between">
-                <ButtonPrimary>Add Goal</ButtonPrimary>
-                <Button>Cancel</Button>
+            <div>
+            {props.children}
             </div>
         </div>
     </div>;

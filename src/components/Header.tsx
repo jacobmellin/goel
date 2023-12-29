91e6dfcd0244a9af8fb7772c9da13745dee2459f
Jacob Mellin <mail@jacobmellin.de>
2023-12-29 15:26:49 +0100

    Make titlebar draggable

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Logo from "../assets/logo.svg";
 import { exit } from '@tauri-apps/api/process';
 
 export default function Header() {
-    return <div class="px-6 pt-6 select-none cursor-default relative z-10">
+    return <div data-tauri-drag-region class="px-6 pt-6 select-none cursor-default relative z-10">
         <a href="#" onClick={async () => { await exit(1); alert("hi"); }} class="close-btn absolute bg-red-400 right-4 top-4 rounded-full w-3 h-3 hover:bg-red-200 transition-colors"></a>
         <a target="_blank"
             href="https://github.com/jacobmellin/goel"
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,7 +10,7 @@ interface ModalProps {
 export default function Modal(props: ModalProps) {
     return <div classList={{ hidden: !props.visible }} class="flex px-4 flex-col justify-center items-center absolute pt-24 top-1 left-1 bottom-1 right-1 rounded-2xl backdrop-blur-lg bg-slate-900/10 select-none cursor-default">
         <div class="p-4 bg-gaze-900 rounded-lg w-full max-w-xl shadow-lg border border-white/10">
-            <h2 class="font-bold text-md text-soothe-400">{props.title}</h2>
+            <h2 class="font-bold text-md text-orange-200">{props.title}</h2>
             <div class="flex gap-2 mt-4 flex-row justify-space-between">
                 <ButtonPrimary>Add Goal</ButtonPrimary>
                 <Button>Cancel</Button>

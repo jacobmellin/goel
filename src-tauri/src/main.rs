77d07b4c86d8100f45eb20acd6d84e1ecd7e5e27
Jacob Mellin <mail@jacobmellin.de>
2023-12-30 16:50:56 +0100

    Add boilerplate functions for backend api

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,20 +1,40 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_goals() -> String {
+    format!("This will contain all the goals. yay!")
 }
 
 #[tauri::command]
-fn get_goals(id: &str) -> String {
-    format!("This will contain all the goals. yay!")
+fn new_goal(goal_raw: &str) -> &str {
+    "id"
+}
+
+#[tauri::command]
+fn get_goal(id: &str) -> &str {
+    "goal"
+}
+
+#[tauri::command]
+fn update_goal(id: &str, goal_raw: &str) {
+
+}
+
+#[tauri::command]
+fn remove_goal(id: &str) {
+
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            get_goals, 
+            new_goal,
+            get_goal,
+            update_goal,
+            remove_goal
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }

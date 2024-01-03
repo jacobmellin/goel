82b1e152977ad41c66f8f4f1f1eb1282d81085b8
Jacob Mellin <mail@jacobmellin.de>
2024-01-03 17:52:11 +0100

    Add inner shadow to input elements

--- src/components/TextareaInput.tsx
+++ src/components/TextareaInput.tsx
@@ -10,6 +10,6 @@ export default function TextareaInput(props: TextInputProps) {
         <div class="text-gaze-400 mb-1 left-0.5 relative text-sm font-bold">{ props.label }</div>
         <textarea 
             onChange={(e) => props.onChange(e.currentTarget.value)}
-            required={props.required} placeholder={props.placeholder} class="text-gaze-300 w-full bg-gaze-800 py-1 px-2 rounded border border-white/10 focus:outline-none box-border focus:border focus:border-orange-300 transition-colors hover:bg-gaze-700 placeholder-gaze-700"></textarea>
+            required={props.required} placeholder={props.placeholder} class="min-h-20 text-gaze-300 w-full bg-gaze-800 py-1 px-2 rounded border border-white/10 focus:outline-none box-border focus:border focus:border-orange-300 transition-colors hover:bg-gaze-700 placeholder-gaze-700 shadow-inner"></textarea>
     </div>;
 }

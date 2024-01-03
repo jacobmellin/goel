b570827b500e9234ead1e2e1a7c99d81cc3b2bef
Jacob Mellin <mail@jacobmellin.de>
2024-01-03 21:04:01 +0100

    Allow saving of goal reflections

--- src/views/TrackProgressView.tsx
+++ src/views/TrackProgressView.tsx
@@ -1,16 +1,34 @@
 import { For } from "solid-js";
 import GoalReflectForm from "../components/GoalReflectForm";
 import { useGoals } from "../store/goals";
+import { invoke } from "@tauri-apps/api";
+import { useInfoBar } from "../components/InfoBar";
 
 export default function TrackProgressView() {
     {/* TODO: Get only goals pending tracking */}
     const [goals,] = useGoals();
+    const infoBar = useInfoBar();
 
     return <div class="mt-4">
         <h1 class="font-bold text-lg text-soothe-400">Track Goal Progress (0/{goals()?.length})</h1>
         <p class="text-sm text-calm-400">Please reflect on your progress with the following goals:</p>
         <For each={goals()}>
-            {(goal) => <GoalReflectForm goal={goal} />}
+            {(goal) => 
+                <GoalReflectForm
+                    finished={false}
+                    onSkip={() => {}}
+                    onSubmit={async (reflectData) => {
+                        try {
+                            await invoke("create_goal_rating", { 
+                                goalId: goal.id,
+                                ratingData: JSON.stringify(reflectData)
+                            }); 
+                            infoBar.showInfo("Saved reflection for goal!");
+                        } catch(e: any) {
+                            infoBar.showError(e.toString());
+                        }
+                    }}
+                    goal={goal} />}
         </For>
     </div>;
 }

7dbf684c02c328ac59c5647dd67b2e2e282cc743
Jacob Mellin <mail@jacobmellin.de>
2024-01-14 21:42:26 +0100

    Add icons for time picker

--- src/components/TimePicker.tsx
+++ src/components/TimePicker.tsx
@@ -7,11 +7,21 @@ interface TimePickerProps {
     onChange: (v: string) => void
 }
 
+// TODO:
+//
+// - Add validation
+// - Dropdown select
+// - Time format based on locale
+// - Focus of container element
+
 export default function TimePicker(props: TimePickerProps) {
-    return <div class="inline-flex w-20 text-gaze-300 bg-gaze-800 py-1 px-2 rounded border border-white/10 focus:outline-none box-border focus:border focus:border-orange-300 transition-colors hover:bg-gaze-700 placeholder-gaze-700 shadow-inner">
+    return <div class="inline-flex w-28 text-gaze-300 bg-gaze-800 py-1 pl-2 rounded border border-white/10 focus:outline-none box-border focus:border focus:border-orange-300 transition-colors hover:bg-gaze-700 placeholder-gaze-700 shadow-inner">
         <EvaIcon name="clock-outline" class="w-6 mr-1.5 fill-soothe-400" />
         <input 
             onChange={(e) => props.onChange(e.currentTarget.value)}
             type="text" value="11:45" pattern="[0-9]{2}:[0-9]{2}" required={props.required} placeholder={props.placeholder} class="w-full text-gaze-300 bg-transparent focus:outline-none box-border focus:border-none focus:border-orange-300 transition-colors hover:bg-gaze-700 placeholder-gaze-700 shadow-inner" />
+            <button type="button" class="inline-flex w-10 h-6 items-center justify-center">
+                <EvaIcon name="arrow-ios-downward-outline" class="w-4 fill-gaze-400" /> 
+            </button>
     </div>;
 }
--- src/views/SettingsView.tsx
+++ src/views/SettingsView.tsx
@@ -1,4 +1,3 @@
-import NumberInput from "../components/NumberInput";
 import TimePicker from "../components/TimePicker";
 
 export default function SettingsView() {
@@ -10,7 +9,7 @@ export default function SettingsView() {
                 <p class="text-soothe-400 text-sm max-w-xs">Goel will automatically remind you at 10:00 of your goals for which reflection on your progress is due.</p>
             </div>
             <div class="flex">
-                <TimePicker />
+                <TimePicker onChange={(v) => console.log(v)} />
             </div>
         </div>
         <div class="bg-gaze-700/50 my-4 rounded-md px-4 py-4 flex justify-between items-center">

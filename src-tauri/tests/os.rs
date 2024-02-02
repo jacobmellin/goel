b3436ce45c055758be576d4463755e1ce566cc15
Jacob Mellin <mail@jacobmellin.de>
2024-02-02 13:41:57 +0100

    Add rust test to check if appdata is created

---
+++ src-tauri/tests/os.rs
@@ -0,0 +1,14 @@
+use std::fs;
+
+use goel::db::establish_connection;
+use goel::config;
+
+#[test]
+fn user_app_cache_created_when_init_db() {
+    let cfg: crate::config::GoelConfig = confy::load("goel", None).unwrap();
+    let db_dir = cfg.db_dir;
+
+    establish_connection().unwrap(); 
+
+    assert!(fs::metadata(db_dir).unwrap().is_dir());
+}

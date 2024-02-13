bc69f74c131da9736e3e395c96e7b5e64b290ea7
Jacob Mellin <mail@jacobmellin.de>
2024-02-13 15:50:41 +0100

    Change reminder switch styling

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -13,14 +13,14 @@ export default function Switch(props: SwitchProps) {
         setChecked(!checked());
         props.onChange(checked());
     }}>
-        <div class="w-10 h-5 border-white/30 relative transition-colors border rounded-full" classList={{
+        <div class="w-12 h-6 border-white/30 relative transition-colors border rounded-full" classList={{
             "bg-soothe-400": checked() ,
             "bg-gaze-800": !checked()
         }}>
-            <span class="w-3.5 h-3.5 absolute transition-all top-[2px] block rounded-full"
+            <span class="w-4 h-4 absolute transition-all top-[3px] block rounded-full"
                 classList={{ 
-                    "bg-gaze-800 left-[22px]": checked(),
-                    "bg-soothe-400 left-[2px]": !checked()
+                    "bg-gaze-800 left-[27px]": checked(),
+                    "bg-soothe-400 left-[4px]": !checked()
                 }} 
             ></span>
             </div>
